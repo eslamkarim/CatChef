2c3efe0153e53dac7500df127aa8cbcc5d642008
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-02-07 18:07:01 +0200

    create webpage cover

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+
+
+// var canvas = document.getElementById('curveCanvas')
+
+// var ctx = canvas.getContext('2d');
+// // Draw shapes
+// ctx.beginPath();
+// /*ctx.moveTo(0,500);
+// ctx.bezierCurveTo(0, 400, 200, 400, 800, 300);*/
+
+// ctx.moveTo(0, 200);
+// ctx.bezierCurveTo(0, 400, 700, 400, 700, 200);
+// ctx.stroke();
