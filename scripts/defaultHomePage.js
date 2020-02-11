03d1bbf03d8b51591c13694b5f823b32c9e21e8a
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-02-11 16:56:53 +0200

    Display random slide show
    there are 2problems ,, 1st problem the size of images ,, 2nd problem finding error with while loop

--- scripts/defaultHomePage.js
+++ scripts/defaultHomePage.js
@@ -3,21 +3,45 @@ var recipesArray = JSON.parse(recipe);
 
 var recipeDay = document.getElementById('recipeDay');
 var recipeImg1 = document.getElementById('recipeImg1');
- var recipeTitle1 = document.getElementById('recipeTitle1');
-var recipeText1 = document.getElementById('recipeText1');
+var slideShow = document.getElementsByClassName('card-img-top');
+var recipeTitle = document.getElementsByClassName('card-title');
+var recipeText = document.getElementsByClassName('card-text');
 
+var i=1 , x;
 
+function displayRecipe() {
 
-function displayRecipe(){ 
-        
-        var randomRecipes =Math.round( Math.random() * recipesArray.length);
+        var randomRecipes = Math.round(Math.random() * recipesArray.length);
 
         recipeImg1.setAttribute("src", recipesArray[randomRecipes].image);
-        recipeTitle1.textContent= recipesArray[randomRecipes].name;
-        recipeText1.textContent= recipesArray[randomRecipes].description;
+        recipeTitle[0].textContent = recipesArray[randomRecipes].name;
+        recipeText[0].textContent = recipesArray[randomRecipes].description;
         console.log(recipesArray[randomRecipes].image);
 
 }
 setInterval(displayRecipe, 3000);
 
-// console.log(recipesArray[5]);
+function displaySlideShow(){
+
+        while(true){
+                
+
+                // for (i = 1; i <= 3; i++) {
+                        var randomRecipes = Math.round(Math.random() * recipesArray.length);
+                        slideShow[i-1].setAttribute("src", recipesArray[randomRecipes].image);
+                        recipeTitle[i].textContent = recipesArray[randomRecipes].name;
+                        recipeText[i].textContent = recipesArray[randomRecipes].description; 
+                
+                // }
+                i++;
+
+        }
+}
+
+$('#multi-item-example').on('slide.bs.carousel', function () {
+        setInterval(displaySlideShow, 200);
+        })
+
+// $('#multi-item-example').carousel({
+//         interval: 20
+// })
