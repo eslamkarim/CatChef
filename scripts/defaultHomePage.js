3c1e2b9f00b12faeb94c60a89a1bd78ceee47b86
Eslam karim gaber <eslamkarim222@gmail.com>
2020-02-11 22:52:56 +0200

    fixing scroll issues and tweaking scroll design

--- scripts/defaultHomePage.js
+++ scripts/defaultHomePage.js
@@ -8,40 +8,41 @@ var recipeTitle = document.getElementsByClassName('card-title');
 var recipeText = document.getElementsByClassName('card-text');
 
 var i=1 , x;
+var slide=1;
+// function displayRecipe() {
 
-function displayRecipe() {
+//         var randomRecipes = Math.round(Math.random() * recipesArray.length);
 
-        var randomRecipes = Math.round(Math.random() * recipesArray.length);
-
-        recipeImg1.setAttribute("src", recipesArray[randomRecipes].image);
-        recipeTitle[0].textContent = recipesArray[randomRecipes].name;
-        recipeText[0].textContent = recipesArray[randomRecipes].description;
-        console.log(recipesArray[randomRecipes].image);
-
-}
-setInterval(displayRecipe, 3000);
+//         recipeImg1.setAttribute("src", recipesArray[randomRecipes].image);
+//         recipeTitle[0].textContent = recipesArray[randomRecipes].name;
+//         recipeText[0].textContent = recipesArray[randomRecipes].description;
+// }
+// setInterval(displayRecipe, 3000);
 
 function displaySlideShow(){
-
-        while(true){
-                
-
-                // for (i = 1; i <= 3; i++) {
+        if(slide == 1){        
+                for (var i = 0; i < 3; i++) {
                         var randomRecipes = Math.round(Math.random() * recipesArray.length);
-                        slideShow[i-1].setAttribute("src", recipesArray[randomRecipes].image);
+                        slideShow[i].setAttribute("src", recipesArray[randomRecipes].image);
                         recipeTitle[i].textContent = recipesArray[randomRecipes].name;
                         recipeText[i].textContent = recipesArray[randomRecipes].description; 
-                
-                // }
-                i++;
-
+                }
+                slide = 2;
+        }else{
+                for (var i = 3; i < 6; i++) {
+                        var randomRecipes = Math.round(Math.random() * recipesArray.length);
+                        slideShow[i].setAttribute("src", recipesArray[randomRecipes].image);
+                        recipeTitle[i].textContent = recipesArray[randomRecipes].name;
+                        recipeText[i].textContent = recipesArray[randomRecipes].description; 
+                }
+                slide = 1;
         }
 }
 
 $('#multi-item-example').on('slide.bs.carousel', function () {
-        setInterval(displaySlideShow, 200);
-        })
+        displaySlideShow();
+})
 
-// $('#multi-item-example').carousel({
-//         interval: 20
-// })
+ $('#multi-item-example').carousel({
+         interval: 4000
+ })
