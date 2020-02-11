c69f2428aedbbff22222520ffce21c1cc520a8d1
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-02-11 14:32:35 +0200

    new structure to load files, fixed few issues

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,19 @@
+var content = document.getElementById("content");
+$('#content').load('../html/landing-page.html #container');
 
+// function load_home() {
+//     content.innerHTML = '<object type="text/html" data="../html/landing-page.html"></object>';
+// }
+// content.addEventListener("load", load_home)
+// content.style.width = window.innerWidth;
+// var html;
+// $.get('../html/landing-page.html', function (data) {
+//     html = data;
+// }).done(function () {
+//     console.log(html);
+// });
+
+// load_home();
 
 // var canvas = document.getElementById('curveCanvas')
 
