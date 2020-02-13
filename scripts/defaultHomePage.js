3e6a451b3d0280bfefbe52ac000099f19b632287
Eslam karim gaber <eslamkarim222@gmail.com>
2020-02-14 00:40:51 +0200

    adding working details button to homepage

--- scripts/defaultHomePage.js
+++ scripts/defaultHomePage.js
@@ -6,7 +6,7 @@ var recipeImg1 = document.getElementById('recipeImg1');
 var slideShow = document.getElementsByClassName('card-img-top');
 var recipeTitle = document.getElementsByClassName('card-title');
 var recipeText = document.getElementsByClassName('card-text');
-
+var detailsButtons = document.getElementsByClassName('btn btn-primary')
 var i = 1, x;
 var slide = 1;
 // function displayRecipe() {
@@ -26,14 +26,16 @@ function displaySlideShow() {
                         slideShow[i].setAttribute("src", recipesArray[randomRecipes].image);
                         recipeTitle[i].textContent = recipesArray[randomRecipes].name;
                         recipeText[i].textContent = recipesArray[randomRecipes].description;
+                        detailsButtons[i].setAttribute("href","../html/specific_recipe.html?name="+recipesArray[randomRecipes].name);
                 }
                 slide = 2;
-        } else {
+        }else{
                 for (var i = 3; i < 6; i++) {
                         var randomRecipes = Math.round(Math.random() * recipesArray.length);
                         slideShow[i].setAttribute("src", recipesArray[randomRecipes].image);
                         recipeTitle[i].textContent = recipesArray[randomRecipes].name;
                         recipeText[i].textContent = recipesArray[randomRecipes].description;
+                        detailsButtons[i].setAttribute("href","../html/specific_recipe.html?name="+recipesArray[randomRecipes].name);
                 }
                 slide = 1;
         }
