0e97aa4dc3ad459ccb101e190e689f51a117664a
AymanMagdy <magdia883@gmail.com>
2020-02-14 13:21:46 +0200

    test changes for log in

--- scripts/Sign_In_Handler.js
+++ scripts/Sign_In_Handler.js
@@ -11,11 +11,11 @@ document.getElementById("sign_in").addEventListener("click", function(event){
     } else {
        sign_in_result = sign_in(email, password);
         if (sign_in_result)
-            alert("signed in");
-        else
-            alert("Wrong username or password.") 
+            console.log("Signed in");
+        else{
+            console.log("Wrong username or password.");
+        }
     }
-    
 })
 
 function sign_in(email, password){
@@ -26,8 +26,15 @@ function sign_in(email, password){
     for(i=0; i < local_storage_keys.length; i++){
         users[i] = JSON.parse(window.localStorage.getItem(local_storage_keys[i]));
         if (email == users[i].email && password == users[i].password){
+            // add the object to the web session.
+            console.log(users[i]);
             return true;
         }
     }
     return false;
+}
+
+function clear_sign_in_feilds(){
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
 }
