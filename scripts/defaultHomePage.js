33a2022571e91ee54045d13df04cc5bdb42ed6ce
Eslam karim gaber <eslamkarim222@gmail.com>
2020-02-16 23:38:17 +0200

    new card style on homepage

--- scripts/defaultHomePage.js
+++ scripts/defaultHomePage.js
@@ -8,10 +8,11 @@ var recipeImg1 = document.getElementById('recipeImg1');
 var slideShow = document.getElementsByClassName('card-img-top');
 var recipeTitle = document.getElementsByClassName('card-title');
 var recipeText = document.getElementsByClassName('card-text');
-var detailsButtons = document.getElementsByClassName('btn btn-primary');
+var recipe_links = document.getElementsByName('recipe_link');
 var cardimages = document.getElementsByClassName("card-img-top");
 var cardTitles =  document.getElementsByClassName("card-title");
 var cardText =  document.getElementsByClassName("card-text");
+var stars = document.getElementsByClassName('stars-active');
 var i = 1, x;
 var slide = 1;
 //initial slider random recipes
@@ -19,36 +20,30 @@ for (let i = 0; i < 6; i++) {
         var rand = Math.round(Math.random() * recipesArray.length);
         cardimages[i].setAttribute("src", recipesArray[rand].image);
         cardTitles[i].textContent = recipesArray[rand].name;
+        recipe_links[i].setAttribute("href","../html/specific_recipe.html?name="+recipesArray[rand].name);
         cardText[i].textContent= recipesArray[rand].description;
-        detailsButtons[i].setAttribute("href","../html/specific_recipe.html?name="+recipesArray[rand].name);
+        stars[i].setAttribute("style","width:"+recipesArray[rand].rating*20+"%");
 }
-// function displayRecipe() {
-
-//         var randomRecipes = Math.round(Math.random() * recipesArray.length);
-
-//         recipeImg1.setAttribute("src", recipesArray[randomRecipes].image);
-//         recipeTitle[0].textContent = recipesArray[randomRecipes].name;
-//         recipeText[0].textContent = recipesArray[randomRecipes].description;
-// }
-// setInterval(displayRecipe, 3000);
 
 function displaySlideShow() {
         if (slide == 1) {
                 for (var i = 0; i < 3; i++) {
                         var randomRecipes = Math.round(Math.random() * recipesArray.length);
                         slideShow[i].setAttribute("src", recipesArray[randomRecipes].image);
-                        recipeTitle[i].textContent = recipesArray[randomRecipes].name;
-                        recipeText[i].textContent = recipesArray[randomRecipes].description;
-                        detailsButtons[i].setAttribute("href","../html/specific_recipe.html?name="+recipesArray[randomRecipes].name);
+                        cardTitles[i].textContent = recipesArray[randomRecipes].name;
+                        recipe_links[i].href="../html/specific_recipe.html?name="+recipesArray[randomRecipes].name;
+                        cardText[i].textContent= recipesArray[randomRecipes].description;
+                        stars[i].setAttribute("style","width:"+recipesArray[randomRecipes].rating*20+"%");
                 }
                 slide = 2;
         }else{
                 for (var i = 3; i < 6; i++) {
                         var randomRecipes = Math.round(Math.random() * recipesArray.length);
                         slideShow[i].setAttribute("src", recipesArray[randomRecipes].image);
-                        recipeTitle[i].textContent = recipesArray[randomRecipes].name;
-                        recipeText[i].textContent = recipesArray[randomRecipes].description;
-                        detailsButtons[i].setAttribute("href","../html/specific_recipe.html?name="+recipesArray[randomRecipes].name);
+                        cardTitles[i].textContent = recipesArray[randomRecipes].name;
+                        recipe_links[i].href="../html/specific_recipe.html?name="+recipesArray[randomRecipes].name;
+                        cardText[i].textContent= recipesArray[randomRecipes].description;
+                        stars[i].setAttribute("style","width:"+recipesArray[randomRecipes].rating*20+"%");
                 }
                 slide = 1;
         }
