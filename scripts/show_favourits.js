396555f1d66915b0531916544e448f61f721b0ff
Eslam karim gaber <eslamkarim222@gmail.com>
2020-02-17 00:14:18 +0200

    fixing favourite page

--- scripts/show_favourits.js
+++ scripts/show_favourits.js
@@ -4,28 +4,41 @@ function user_show_favs() {
     var user_id = user_ida[1];
     var user = JSON.parse(sessionStorage.getItem(user_id));
     var user_favs = user.favourits;
-
+    
     let resultList = document.getElementById('favourtis');
     resultList.innerHTML = "";
-    resultList.innerHTML += `<div class="row">`    
-    user_favs.forEach(element => {
-        console.log(element);
-        recipe = SearchByRecipeName(element)
-        
-        resultList.innerHTML += `
-                    <div class="col-md-3">
-                        <div class="card mb-2 h-30 w-30">
-                            <img class="card-img-top" src="${recipe.image}" alt="Card image cap">
-                            <div class="card-body">
-                            <h4 class="card-title">${recipe.name}</h4>
-                            <p class="card-text">${recipe.description}</p>
-                            <a class="btn btn-primary" href="../html/specific_recipe.html?name=${recipe.name}">Details</a>
-                            </div>
+    for (let i = 0; i < user_favs.length; i++) {
+        var user_recipe=SearchByRecipeName(user_favs[i]);
+        resultList.innerHTML +=
+        `<div class="col col-sm-6 col-md-4 col-lg-3 list-item h-25 my-3 mx-auto">
+            <div class="card w-30 h-30 shadow">
+                <img class="card-img-top" src=${user_recipe.image} alt="Card image cap">
+                <div class="card-body">
+                    <span class="score">
+                        <div class="score-wrap">
+                            <span class="stars-active" style="width:${user_recipe.rating * 20}%">
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                            </span>
+                            <span class="stars-inactive">
+                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                            </span>
                         </div>
-                    </div>`;
-    },
-        resultList.innerHTML += `</div>`,
-    );
+                    </span>
+                    <h5 class="card-title"><a
+                            href="../html/specific_recipe.html?name=${user_recipe.name}">${user_recipe.name}</a></h6>
+                        <p class="card-text overflow-hidden">${user_recipe.description}</p>
+                </div>
+            </div>
+        </div>`
+    }
 }
 
 user_show_favs()
